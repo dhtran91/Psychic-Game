c1a8fe4027edd4652cb9c8fc9baca3d49f760a4f
David Tran <dhtran@hotmail.com>
2018-05-14 14:49:07 -0500

    Workable game but still need to fix if user presses same key

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,68 @@
-
-
 //Array that consist of 26 characters ranging from index 0 - 25
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Generate a number between 0 - 25
 var rand = Math.floor(Math.random() * 25);
+
+//Just to see what the random letter is
 console.log(alphabet[rand]);
+
+//Setting global variables
 var numOfGuesses = 10;
 var win = 0;
 var loss = 0;
-var guesses = [];
+var guesses;
+var keysPressed = [];
+var winText = document.getElementById("win");
+var lossText = document.getElementById("loss");
+var numOfGuessesText = document.getElementById("numOfGuesses");
+var guessText = document.getElementById("guesses");
+var correctLetterText = document.getElementById("correctLetter");
 
-// document.getElementById("numOfGuesses").innerHTML = numOfGuesses;
-// document.getElementById("guesses").innerHTML = guesses;
-// document.getElementById("win").innerHTML = win;
-// document.getElementById("loss").innerHTML = loss;
+//Displaying inital variables to html
+winText.textContent = win;
+lossText.textContent = loss;
+numOfGuessesText.textContent = numOfGuesses;
 
-  
-var keyStroke;
-document.addEventListener('keypress', (event) => {
-    keyStroke = event.key;
-    alert("You pressed " + keyStroke);
-});
+function reset() {
+correctLetterText.textContent = correctLetter;
+this.numOfGuesses = 10;
+this.rand = Math.floor(Math.random() * 25);
+guessText.textContent = "";
+console.log(alphabet[rand]);
+keysPressed = [];
+};
 
-while (numOfGuesses !== 0) {
+document.onkeypress = function (event) {
+numOfGuesses--;
+numOfGuessesText.textContent = numOfGuesses;
+correctLetterText.textContent = "";
+correctLetter = alphabet[rand];
 
-    if (alphabet[rand] === keyStroke) {
-        alert("You win");
-        win++;
-        numOfGuesses = 0;
-    } 
-    else {
-    guesses.push(userInput);
-    numOfGuesses--;
-    if (numOfGuesses == 0) {
-        loss++;
-    }
-    }
-    // console.log(win);
-    // console.log(loss);
-    // console.log(numOfGuesses);
+var keyPressed = event.key;
+
+//Can't push event
+// keysPressed = keysPressed.push(keyPressed);
+
+// for (var i = 0; i < keysPressed.length; i++) {
+//     if (keysPressed[i] === keyPressed) {
+//         keyPressed = "";
+//     } else {
+        
+//     }
+// };
+
+guessText.textContent += keyPressed + " ";
+
+if (numOfGuesses >= 0 && correctLetter === keyPressed) {
+    win++;
+    winText.textContent = win;
+    reset();
 }
-
-
-
-
-
-
+else if (numOfGuesses === 0) {
+    loss++;
+    lossText.textContent = loss;
+    reset();
+}
+}
